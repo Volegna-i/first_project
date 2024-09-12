3a0d0ce8cda6b3cdde0328ae9be2ed723b42135f
I.V. Angelov <angelov.i.v@yandex.ru>
2024-09-12 23:32:41 +0300

    make some fixes for maket

--- script.js
+++ script.js
@@ -1,46 +1,16 @@
-const elements = document.querySelectorAll('.menu__item');  
-const showAllButton = document.querySelector('.showAll');  
+document.addEventListener('DOMContentLoaded', function() {
+  const showAllButton = document.querySelector('.showAll');
+  const toggleMenu = document.querySelector('.menuBox .swiper-wrapper');
 
-let maxVisibleElements;  
-
-function updateVisibleElements() {
-  const screenWidth = window.innerWidth;  
-  maxVisibleElements = screenWidth < 1119 ? 6 : 8;
-  hideElements(); 
-}
-
-function hideElements() {  
-  for (let elem = 0; elem < elements.length; elem++) {  
-    if (elem >= maxVisibleElements) {  
-      elements[elem].classList.add('hidden');  
-    } else {
-      elements[elem].classList.remove('hidden');
-    }  
-  }  
-  showAllButton.textContent = 'Показать все';  
-};  
-
-function showElements() {
-    if (showAllButton.textContent === 'Показать все') {
-      for (let elem = 0; elem < elements.length; elem++) {
-        elements[elem].classList.remove('hidden');
+  showAllButton.addEventListener('click', function() {
+      if (toggleMenu.classList.contains('toggleVisibility')) {
+          toggleMenu.classList.remove('toggleVisibility');
+          showAllButton.textContent = 'Скрыть';
+          showAllButton.classList.add('show-hidden');
+      } else {
+          toggleMenu.classList.add('toggleVisibility');
+          showAllButton.textContent = 'Показать всё';
+          showAllButton.classList.remove('show-hidden');
       }
-      showAllButton.textContent = 'Скрыть';
-      showAllButton.classList.add('show-hidden');
-    } else {
-      for (let elem = 0; elem < elements.length; elem++) {
-        if (elem >= maxVisibleElements) {
-          elements[elem].classList.add('hidden');
-        }
-      }
-      showAllButton.textContent = 'Показать все';
-      showAllButton.classList.remove('show-hidden');
-    }
-  }
-  
-
-
-  updateVisibleElements();
-  window.addEventListener('resize', updateVisibleElements); 
-  
-  showAllButton.addEventListener('click', showElements); 
+  });
+});
