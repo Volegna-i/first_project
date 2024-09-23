6d250b84e9431f83d99bb9e16ab43e44555a9c38
I.V. Angelov <angelov.i.v@yandex.ru>
2024-09-23 20:06:03 +0300

    some fixes

--- src/js/brandSectionShowAll.js
+++ src/js/brandSectionShowAll.js
@@ -1,16 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
   const showAllButton = document.querySelector('.brandSection__showAll');
-  const toggleMenu = document.querySelector('.brandSection__menuBox .swiper-wrapper');
+  const toggleMenu = document.querySelector('.brandSection__menu');
+  const showBrand = document.querySelector('.brandSection__showBrand');
+  const hideBrand = document.querySelector('.brandSection__hideBrand');
 
   showAllButton.addEventListener('click', function() {
-      if (toggleMenu.classList.contains('brandSection--hide')) {
-          toggleMenu.classList.remove('brandSection--hide');
-          showAllButton.textContent = 'Скрыть';
-          showAllButton.classList.add('show-hidden');
-      } else {
-          toggleMenu.classList.add('brandSection--hide');
-          showAllButton.textContent = 'Показать всё';
-          showAllButton.classList.remove('show-hidden');
-      }
+    showAllButton.classList.toggle('show-hidden');
+    toggleMenu.classList.toggle('brandSection--hide');
+    showBrand.classList.toggle('hiddenText');
+    hideBrand.classList.toggle('hiddenText');
   });
 });
--- src/js/typeSectionShowAll.js
+++ src/js/typeSectionShowAll.js
@@ -1,16 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
   const showAllButton = document.querySelector('.typeSection__showAll');
-  const toggleMenu = document.querySelector('.typeSection__menuBox .swiper-wrapper');
+  const toggleMenu = document.querySelector('.typeSection__menu');
+  const showType = document.querySelector('.typeSection__showType');
+  const hideType = document.querySelector('.typeSection__hideType');
 
   showAllButton.addEventListener('click', function() {
-      if (toggleMenu.classList.contains('typeSection--hide')) {
-          toggleMenu.classList.remove('typeSection--hide');
-          showAllButton.textContent = 'Скрыть';
-          showAllButton.classList.add('show-hidden');
-      } else {
-          toggleMenu.classList.add('typeSection--hide');
-          showAllButton.textContent = 'Показать всё';
-          showAllButton.classList.remove('show-hidden');
-      }
+    showAllButton.classList.toggle('show-hidden');
+    toggleMenu.classList.toggle('typeSection--hide');
+    showType.classList.toggle('hiddenText');
+    hideType.classList.toggle('hiddenText');
   });
 });
